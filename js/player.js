59ce00b778f70f4d1a2ca5d1ffaab821d6ec9bc9
lucie <lucie.mzt@gmail.com>
2020-11-10 23:54:00 +0100

    not mvp

--- js/player.js
+++ js/player.js
@@ -5,7 +5,7 @@ class Player {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
     this.x = 350;
-    this.y = 520;
+    this.y = 5;
     this.speed = 2;
     this.lives = 3;
     this.score = 0;
@@ -15,14 +15,13 @@ class Player {
   update() {
     this.x = this.x + this.direction * this.speed;
   }
-  
+
   drawPlayer() {
-    console.log("player",this.x)
+    console.log("player", this.x);
     //this.playerImage.onload = (this.playerImage,this.x, this.y, 60, 60);
     this.playerImage.src =
       "https://i.pinimg.com/564x/7b/fa/a4/7bfaa42e0fc93da2b3fed34ef20e4cc9.jpg";
     this.ctx.drawImage(this.playerImage, this.x, this.y, 60, 60);
-
   }
 
   checkScreen() {
@@ -33,7 +32,7 @@ class Player {
     }
   }
 
-  setDirection(direction){
+  setDirection(direction) {
     this.direction = direction;
   }
   loseLive() {
